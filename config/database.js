758b338c3a46696dabf91f833a0cfd0a687f71ec
Santhi Raju <santhiraju32@gmail.com>
2026-02-24 17:57:55 +0530

    directly added the url

--- config/database.js
+++ config/database.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    const conn = await mongoose.connect('mongodb+srv://santhiraju32_db_user:hMvYHRrQvS6TWsRS@cluster0.nysuteb.mongodb.net/?appName=Cluster0', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      
+
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
