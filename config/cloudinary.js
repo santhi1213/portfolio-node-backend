9df128a37618d1c589ead4fb746bd1cc07e8cb2e
santhi1213 <santhiraju32@gmail.com>
2026-02-25 21:54:44 +0530

    added cloudinary keys directly

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'ddkm920ry',
+  api_key: process.env.CLOUDINARY_API_KEY || '353935299157181',
+  api_secret: process.env.CLOUDINARY_API_SECRET || 'ytAjRgEKNk5w7mo5gB1OJWntSsM',
 });
 
 module.exports = cloudinary;
